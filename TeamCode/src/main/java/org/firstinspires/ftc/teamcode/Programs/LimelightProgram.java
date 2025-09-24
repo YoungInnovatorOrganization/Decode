e1dde7678fe99ecd0fb078e1ea5caffb5ab2228f
brontobbyte <brontobyteftc@gmail.com>
2025-09-24 10:40:04 -0300

    Update LimelightProgram.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Programs/LimelightProgram.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Programs/LimelightProgram.java
@@ -1,4 +1,5 @@
 package org.firstinspires.ftc.teamcode.Programs;
+import com.acmerobotics.dashboard.FtcDashboard;
 import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.hardware.limelightvision.LLResultTypes;
 import com.qualcomm.hardware.limelightvision.LLStatus;
@@ -16,29 +17,42 @@ import java.util.List;
 
 import dev.nextftc.ftc.NextFTCOpMode;
 
-@TeleOp(name = "NextFTC TeleOp Program Java")
+@TeleOp(name = "LimelightProgram")
 public class LimelightProgram extends NextFTCOpMode {
     Limelight3A limelight;
-    LLResult result = limelight.getLatestResult();
     //FtcDashboard dashboard = FtcDashboard.getInstance();
 
-
-    public LimelightProgram() {
+    @Override
+    public void onInit() {
+        FtcDashboard.getInstance().startCameraStream(limelight, 60);
         limelight = hardwareMap.get(Limelight3A.class, "limelight");
+        telemetry.setMsTransmissionInterval(11);
         limelight.setPollRateHz(100); // This sets how often we ask Limelight for data (100 times per second)
         limelight.start(); // This tells Limelight to start looking!
         limelight.pipelineSwitch(1); // Switch to pipeline number 0
         limelight.start();
-        //FtcDashboard.getInstance().startCameraStream(limelight, 0);
     }
+
     @Override
     public void onStartButtonPressed() {
-        result.getPipelineIndex();
+        LLStatus status = limelight.getStatus();
+        telemetry.addData("Name", "%s",
+                status.getName());
+        telemetry.addData("LL", "Temp: %.1fC, CPU: %.1f%%, FPS: %d",
+                status.getTemp(), status.getCpu(),(int)status.getFps());
+        telemetry.addData("Pipeline", "Index: %d, Type: %s",
+                status.getPipelineIndex(), status.getPipelineType());
         LLResult result = limelight.getLatestResult();
-        List<LLResultTypes.FiducialResult> fiducials = result.getFiducialResults();
-        List<LLResultTypes.FiducialResult> fiducialResults = result.getFiducialResults();
-        for (LLResultTypes.FiducialResult fr : fiducialResults) {
-            telemetry.addData("Fiducial", "ID: %d, Family: %s, X: %.2f, Y: %.2f", fr.getFiducialId(), fr.getFamily(), fr.getTargetXDegrees(), fr.getTargetYDegrees());
+        if (result.isValid()) {
+            result.getPipelineIndex();
+            List<LLResultTypes.FiducialResult> fiducials = result.getFiducialResults();
+            List<LLResultTypes.FiducialResult> fiducialResults = result.getFiducialResults();
+            for (LLResultTypes.FiducialResult fr : fiducialResults) {
+                telemetry.addData("Fiducial", "ID: %d, Family: %s, X: %.2f, Y: %.2f", fr.getFiducialId(), fr.getFamily(), fr.getTargetXDegrees(), fr.getTargetYDegrees());
+            }
+        } else {
+            telemetry.addData("Limelight", "No data available");
         }
+        telemetry.update();
     }
 }
