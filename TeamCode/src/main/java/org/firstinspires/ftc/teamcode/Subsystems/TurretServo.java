6599244667aaddf3151c82ccb55983e6577d517e
brontobbyte <brontobyteftc@gmail.com>
2025-09-29 10:08:47 -0300

    bababa

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/TurretServo.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.Subsystems;
+
+import com.qualcomm.robotcore.hardware.CRServo;
+
+import dev.nextftc.hardware.impl.MotorEx;
+
+public class TurretServo {
+    public static final TurretServo INSTANCE = new TurretServo();
+    private TurretServo() { }
+}
